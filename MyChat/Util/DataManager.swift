1a33b07d5dcaf8a580f0e995bacba85c3693ac91
MPB <mpb@MPB-MacBook-Pro.local>
2018-03-02 11:56:51 +0100

    příjem zpráv z databaze

--- MyChat/Util/DataManager.swift
+++ MyChat/Util/DataManager.swift
@@ -22,11 +22,45 @@ class DataManager{
     
     init() {
       //  delegate?.dataManagerDidReceiveNewData(self)
-        
+        watchForUpdates()
         
     }
     
-
+    //čekování databáze zda je tam změna
+    func watchForUpdates(){  //tato funkce zůstává aktivní a kdykoliv se změní databáze tak se refrešne
+        Database.database().reference(withPath:"Messages").observe(.value) {(snapshot) in
+         //   print(snapshot.children.allObjects)
+            
+            //self.delegate?.dataManagerDidReceiveNewData(self)
+            self.parseData(snapshot)
+            
+        }
+    }
+    
+    func parseData(_ data: DataSnapshot){
+        self.messages.removeAll() // smažu původní data
+        
+        for dictionary in data.children.allObjects{
+           if let json = dictionary as? DataSnapshot,
+            let messageJson = json.value as? [String:Any], //json.value osekne id a vypíše jsonMessage
+            let messageText = messageJson["text"] as? String,
+            let senderJson = messageJson["sender"] as? [String: Any],
+            let senderName = senderJson["name"] as? String{
+            
+            let message = Message(text: messageText, sender: User(name: senderName))
+            messages.append(message)
+            
+            
+            }
+        }
+        
+        
+        messages.reverse() // otočení pole
+        
+        delegate?.dataManagerDidReceiveNewData(self)
+    }
+    
+    //odesílání do databaze
     func sendMessage(_ message: Message){
       //  messages.insert(message, at: 0)
       //  delegate?.dataManagerDidReceiveNewData(self)
